a03f937b42ce44fc93237864971f51539970fca0
coder-srb <soubhagyaranjan.bisoi99@gmail.com>
2024-10-26 23:54:58 +0530

    server without jwt

---
+++ server/routes/residencyRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  createResidency,
+  getAllResidencies,
+  getResidency,
+} from "../controllers/residencyCntrl.js";
+const router = express.Router();
+
+router.post("/create", createResidency);
+router.get("/allresidencies", getAllResidencies);
+router.get("/:id", getResidency);
+
+export { router as residencyRoute };
